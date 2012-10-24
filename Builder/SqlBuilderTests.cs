aea72420800425086ee7ffc7efafe3209e26e2a4
moloean <moloean@gmail.com>
2012-10-24 22:13:35 +0200

    init sqlBuilder

---
+++ Builder/SqlBuilderTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Builder
+{
+    [TestFixture]
+    public class SqlBuilderTests
+    {
+        [Test]
+        public void ToString_Init_ReturnStringEmpty()
+        {
+            // Arrange
+            var sqlBuilder = new SqlBuilder();
+
+            // Act
+            var sqlExpresion = sqlBuilder.ToString();
+
+            // Assert
+            Assert.That(sqlExpresion, Is.Empty.And.Not.Null);
+        }
+ 
+    }
+
+    public class SqlBuilder
+    {
+        public override string ToString()
+        {
+            return string.Empty;
+        }
+    }
+}
